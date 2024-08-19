7e8e438e8c62a697e626d1f3c3952e0922de5d6c
Elham Shahabi <elham.shahabi2@gmail.com>
2024-08-19 19:15:13 +0330

    form feild validetion completed and created reset form function

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,22 +4,26 @@ const email = document.querySelector("#email");
 const massage = document.querySelector("#massage");
 const consent = document.querySelector("#consent");
 const contactForm = document.querySelector("#contact-form");
+const querytypes = document.getElementsByName("query");
+console.log(querytypes);
 
 // select error elm
 const fullNameErrEl = document.querySelector("#fullNameErr");
 const emailErrEl = document.querySelector("#emailErr");
 const msgErrEl = document.querySelector("#msgErr");
 const consentErrEl = document.querySelector("#consentErr");
+const queryErrEl = document.querySelector("#queryErr");
 
 contactForm.addEventListener("submit", (e) => {
   // az reload safhe jlo giri mikone
   e.preventDefault();
-
+  const data = {};
   // validet fullName
   if (fullName.value == "" || fullName.value.length < 3) {
     fullNameErrEl.textContent = "this field is required";
   } else {
     fullNameErrEl.textContent = "";
+    data.fullName = fullName.value;
   }
 
   // validet Email
@@ -32,6 +36,7 @@ contactForm.addEventListener("submit", (e) => {
       emailErrEl.textContent = "Email is not valid !";
     } else {
       emailErrEl.textContent = "";
+      data.email = email.value;
     }
   }
 
@@ -40,6 +45,7 @@ contactForm.addEventListener("submit", (e) => {
     msgErrEl.textContent = "this field is required";
   } else {
     msgErrEl.textContent = "";
+    data.massage = massage.value;
   }
 
   // consent
@@ -47,5 +53,37 @@ contactForm.addEventListener("submit", (e) => {
     consentErrEl.textContent = "this field is required";
   } else {
     consentErrEl.textContent = "";
+    data.consent = consent.value;
+  }
+
+  //   query type
+  let itemChecked = null;
+  querytypes.forEach((item) => {
+    if (item.checked) {
+      itemChecked = item;
+    }
+  });
+  if (!itemChecked) {
+    queryErrEl.textContent = "this field is required";
+  } else {
+    queryErrEl.textContent = "";
+    data.queryType = itemChecked.value;
+  }
+
+  if (
+    data.fullName &&
+    data.consent &&
+    data.email &&
+    data.massage &&
+    data.queryType
+  ) {
+    console.log(data);
+    resetForm();
   }
 });
+
+
+function resetForm() {
+   fullName.value = email.value = massage.value = "";
+   contactForm.reset();
+}
